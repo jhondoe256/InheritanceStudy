2e199bd379660c43061699f68fabd6b7f0832f7f
jhondoe256@yahoo.com <jhondoe256@yahoo.com>
2021-06-11 18:07:31 -0400

    Made The Base Class

---
+++ PokeDex.Data/Pokemon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeDex.Data
+{
+    //This is our 'Base' class.
+    public class Pokemon
+    {
+        //we create our properties that represent our interpretation of what a pokemon should be
+        public string Name { get; set; }
+        public int NationalNumber { get; set; }
+        public string Type { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public List<string> Abilities { get; set; }
+
+        //we make our constructors....this one is empty
+        public Pokemon()
+        {
+
+        }
+
+        //we make our constructors....this one is full
+        public Pokemon(string name, int nationalNumber, string type, string height, string weight, List<string> abilities)
+        {
+            Name = name;
+            NationalNumber = nationalNumber;
+            Type = type;
+            Height = height;
+            Weight = weight;
+            Abilities = abilities;
+        }
+
+    }
+}
